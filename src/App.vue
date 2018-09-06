09919d787eaa5e56733e72e5ac36d0cb01f2f82b
Brad Roodt <brad.roodt@gmail.com>
2018-09-06 20:57:59 +0200

    feat: added call to test contract method send.

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import web3 from './web3';
-import contractInstance from './contractInstance';
+import instance from './contractInstance';
 
 export default {
 	name: 'app',
@@ -18,11 +18,16 @@ export default {
 	async mounted() {
 		const accounts = await web3.eth.getAccounts();
 		const from = accounts[0];
-		const file = await contractInstance.methods
-			.files(
-				'0xc831f48ff2c0a3ffa70380f2bff05f29f37486d896b306dc98a29bfb66c866fb'
-			)
-			.call({ from });
+		const price = web3.utils.toWei('0.2', 'ether');
+		const ipfsHash =
+			'0xc831f48ff2c0a3ffa70380f2bff05f29f37486d896b306dc98a29bfb66c866dd';
+		const tx = await instance.methods.addFile(ipfsHash, price).send({
+			from,
+			gas: '1000000'
+		});
+		console.log(tx);
+
+		const file = await instance.methods.files(ipfsHash).call({ from });
 		console.log(file);
 	}
 };
