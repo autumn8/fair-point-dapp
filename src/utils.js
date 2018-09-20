f4aef24a1014746904a45eba44488c2e8bc94ad0
Brad Roodt <brad.roodt@gmail.com>
2018-09-20 23:13:15 +0200

    feat: added async wrapper to pass back error & data objects from promises

--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,13 @@
-import web3 from '@/web3';
-import axios from 'axios';
-import { getBytes32FromIpfsHash } from '@/ipfsHashConversion';
-import contractInstance from '@/contractInstance';
-
-function sendForm(primaryImage, previewImage, price) {
-	const formData = new FormData();
-	formData.append('primary', primaryImage, 'primary');
-	formData.append('preview', previewImage, 'preview');
-	formData.append('price', price);
-	return axios.post('http://localhost:8080/upload', formData, {
-		headers: {
-			'Content-Type': 'multipart/form-data'
-		}
-	});
+function __(promise) {
+	return promise
+		.then(data => ({ error: null, data }))
+		.catch(error => ({ error, data: null }));
 }
 
-async function addFileToContract(fileID, amount, ethUnit) {
-	debugger;
-	const accounts = await web3.eth.getAccounts();
-	const gas = '1000000';
-	console.log(accounts[0]);
-	const price = web3.utils.toWei(amount, ethUnit);
-	return contractInstance.methods.addFile(fileID, price).send({
-		from: accounts[0],
-		gas
-	});
-}
+// async function runAsync() {
+//   const {data, error} = await __(asyncRequest());
+//   if (error) return console.error('error:', error);
+//   console.log('data:', data);
+// }
 
-async function getFileFromContract(fileID, amount, ethUnit) {
-	const file = await contractInstance.methods
-		.files(fileID)
-		.call({ from: accounts[0] });
-}
-
-export { addFileToContract, sendForm };
+export { __ };
