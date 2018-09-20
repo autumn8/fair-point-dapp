c3403f3407bf16aa5124765f544fe68fd2dc0119
Brad Roodt <brad.roodt@gmail.com>
2018-09-20 23:15:05 +0200

    feat: added error message constants

---
+++ src/errorMessages.js
@@ -0,0 +1,6 @@
+export default {
+	ERROR_ON_IMAGE_UPLOAD:
+		'There was a problem uploading your image. Please try again',
+	ERROR_ADDING_TO_CONTRACT:
+		'There was a problem adding your file to the Eth. Network'
+};
