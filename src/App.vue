1d1d75c88e643fb90da9f2ad16bbe4663cc54589
Brad Roodt <brad.roodt@gmail.com>
2018-09-26 08:12:57 +0200

    feat: Added toolbar with navigation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,38 @@
 <template>
   <v-app>
+    <v-card
+    color="grey lighten-4"
+    flat
+    >
+  <v-toolbar color="grey darken-1" dark>
+    <v-spacer></v-spacer>
+    <v-btn
+      outline color="white"
+      :loading="loading3"
+      :disabled="loading3"
+      class="white--text"
+      to="/"
+      @click.native="loader = 'loading3'"
+    >
+      Upload
+      <v-icon right dark>cloud_upload</v-icon>
+    </v-btn>
+    <v-btn
+      outline color="white"
+      :loading="loading3"
+      :disabled="loading3"
+      class="white--text"
+      to ='/files'
+      @click.native="loader = 'loading3'"
+    >
+      Files
+      <v-icon right dark>file_copy</v-icon>
+    </v-btn>
 
+
+
+  </v-toolbar>
+</v-card>
     <router-view/>
   </v-app>
 </template>
